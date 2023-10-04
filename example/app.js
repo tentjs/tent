e757c729ea60d7d1cb772df4669951ba0595f6b0
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-10-04 21:51:59 +0200

    new diff & render methods

--- example/app.js
+++ example/app.js
@@ -1,51 +1,31 @@
 const App = new Praxy();
 
-// The function would be imported from a separate package,
-// but for the sake of this example, the function is defined in ./extensions.js
-// and included in ./index.html as a script tag
-App.registerExtension(
-  'generateSelectOptions',
-  generateSelectOptions,
-)
-
 const MyComponent = {
   name: 'myComponent',
-  template: html`
-    <div>Hello {{entity}} My name is {{name}}.</div>
-    <div>And I do love the {{easy}} of Praxy.</div>
-    <input name="test-input" />
-    <div>This is the selected value: {{selected}}</div>
-    <div>This is the selected value of select2: {{selected2}}</div>
-  `,
+  template: html`<div>
+    <div>Hello <span>test</span> {{entity}}</div>
+    <div>Testing something simple {{easy}}</div>
+    <button>Click me</button>
+  </div>`,
   data: {
     entity: 'World!',
     easy: 'simplicity',
     name: 'Sebastian',
-    selected: '',
-    selected2: '',
+    nested: {
+      key: 'value',
+    },
   },
 };
 
-App.component(MyComponent, function() {
-  this.on('input', '[name="test-input"]', ({target}) => {
-    this.set('entity', target.value);
+App.component(MyComponent, function(data) {
+  this.on('click', 'button', () => {
+    data.entity = 'Universe!';
+    data.easy = 'complexity';
   });
+  // this.on('input', 'input', ({target}) => {
+  //   data.name = target.value;
+  // });
 });
-
-App
-  .fetch(
-    'https://dummyjson.com/products',
-    {},
-    '[name="select"], [name="select2"]',
-    async ({self, target, res}) => {
-      const {products} = await res.json();
-      self.extensions.generateSelectOptions({
-        target,
-        items: products,
-        map: {value: 'id', text: 'title'}
-      });
-    }
-  );
 
 function html(...values) {
   return values.join('');
