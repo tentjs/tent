802f3108df86fdeb75795b9d5673272ec4f1102b
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-10-14 21:30:03 +0200

    update example

--- example/app.js
+++ example/app.js
@@ -1,9 +1,8 @@
-import {Praxy} from '../dist/praxy';
+import { Praxy } from '../dist/praxy';
 
 const App = new Praxy();
 
-App.component({
-  name: 'todo-list',
+App.component('todo-list', {
   data: {
     newTodo: '',
     todos: [
@@ -12,13 +11,16 @@ App.component({
       { text: 'Build something awesome', done: false }
     ],
   },
-}, ({on, data}) => {
-  on('click', '.delete', ({item}) => {
-    const {todos} = data;
+}, ({ on, data }) => {
+  let todoInput = null;
+
+  on('click', '.delete', ({ item }) => {
+    const { todos } = data;
     data.todos = todos.filter(t => t.text !== item.text);
   });
-  on('click', '.toggle', ({item}) => {
-    const {todos} = data;
+
+  on('click', '.toggle', ({ item }) => {
+    const { todos } = data;
     const todo = todos.find(t => t.text === item.text);
     const index = todos.indexOf(todo);
     todo.done = !todo.done;
@@ -30,14 +32,18 @@ App.component({
     }
     data.todos = todos;
   });
-  on('input', '#add-todo', ({target}) => {
+
+  on('input', '#add-todo', ({ target }) => {
+    todoInput = target;
     data.newTodo = target.value;
   });
+
   on('click', '#add-button', () => {
-    const {todos, newTodo} = data;
+    const { todos, newTodo } = data;
     if (newTodo) {
-      todos.unshift({text: newTodo, done: false});
+      todos.unshift({ text: newTodo, done: false });
       data.todos = todos;
+      todoInput.value = '';
     }
   });
 });
