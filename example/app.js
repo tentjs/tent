208601242ac1100b9bf52d7c3eb7528076d45608
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-10-14 22:08:19 +0200

    prettier

--- example/app.js
+++ example/app.js
@@ -1,50 +1,53 @@
-import { Praxy } from '../dist/praxy';
+import {Praxy} from '../dist/praxy';
 
 const App = new Praxy();
 
-App.component('todo-list', {
-  data: {
-    newTodo: '',
-    todos: [
-      { text: 'Learn JavaScript', done: false },
-      { text: 'Learn Praxy', done: false },
-      { text: 'Build something awesome', done: false }
-    ],
+App.component(
+  'todo-list',
+  {
+    data: {
+      newTodo: '',
+      todos: [
+        {text: 'Learn JavaScript', done: false},
+        {text: 'Learn Praxy', done: false},
+        {text: 'Build something awesome', done: false},
+      ],
+    },
   },
-}, ({ on, data }) => {
-  let todoInput = null;
+  ({on, data}) => {
+    let todoInput = null;
 
-  on('click', '.delete', ({ item }) => {
-    const { todos } = data;
-    data.todos = todos.filter(t => t.text !== item.text);
-  });
+    on('click', '.delete', ({item}) => {
+      const {todos} = data;
+      data.todos = todos.filter((t) => t.text !== item.text);
+    });
 
-  on('click', '.toggle', ({ item }) => {
-    const { todos } = data;
-    const todo = todos.find(t => t.text === item.text);
-    const index = todos.indexOf(todo);
-    todo.done = !todo.done;
-    todos.splice(index, 1);
-    if (todo.done) {
-      todos.push(todo);
-    } else {
-      todos.unshift(todo);
-    }
-    data.todos = todos;
-  });
-
-  on('input', '#add-todo', ({ target }) => {
-    todoInput = target;
-    data.newTodo = target.value;
-  });
-
-  on('click', '#add-button', () => {
-    const { todos, newTodo } = data;
-    if (newTodo) {
-      todos.unshift({ text: newTodo, done: false });
+    on('click', '.toggle', ({item}) => {
+      const {todos} = data;
+      const todo = todos.find((t) => t.text === item.text);
+      const index = todos.indexOf(todo);
+      todo.done = !todo.done;
+      todos.splice(index, 1);
+      if (todo.done) {
+        todos.push(todo);
+      } else {
+        todos.unshift(todo);
+      }
       data.todos = todos;
-      todoInput.value = '';
-    }
-  });
-});
+    });
 
+    on('input', '#add-todo', ({target}) => {
+      todoInput = target;
+      data.newTodo = target.value;
+    });
+
+    on('click', '#add-button', () => {
+      const {todos, newTodo} = data;
+      if (newTodo) {
+        todos.unshift({text: newTodo, done: false});
+        data.todos = todos;
+        todoInput.value = '';
+      }
+    });
+  }
+);
