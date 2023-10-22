4e498a2e6bedd7ee6df4cbe62430b41a5c87c018
sebkolind <sks1993@gmail.com>
2023-10-22 11:36:00 +0200

    Update app2.js

--- example/app2.js
+++ example/app2.js
@@ -1,4 +1,4 @@
-import {L, R} from '../dist/el'
+import {L, R} from '../dist/l'
 
 const Layout = L('div', [
   L('header', [
@@ -21,143 +21,108 @@ const Layout = L('div', [
   L('footer', ['Footer']),
 ])
 
-const Home = L(
-  'div',
-  ({data}) => [
-    L('h1', ['Home']),
-    L(
-      'ul',
-      data.items.map((item) =>
-        L(
-          'li',
-          [
-            L('strong', [item.name]),
-            L('p', [item.description], {style: 'margin: 0'}),
-            L('p', [L('span', [item.subtitle])], {
-              style: 'margin: 0',
-            }),
-          ],
-          {
-            class: 'list-item',
-          }
-        )
-      ),
-      {
-        class: 'items-list',
-        styles: {
-          'list-style': 'none',
-          margin: 0,
-          padding: 0,
-          li: {
-            padding: '8px',
-            color: '#333',
-            background: '#eee',
-            'border-radius': '4px',
-            margin: '0 0 4px 0',
-          },
-        },
-      }
-    ),
-    L('button', [data.amount ? `Clicked ${data.amount} times` : 'Click me'], {
-      onclick() {
-        data.amount = data.amount + 1
-        const items = [...data.items]
-        items[0].name = 'React'
-        items[1].subtitle = 'ReactX'
-        data.items = items
-      },
-    }),
-    L('div', [
-      L('input', [], {
-        type: 'text',
-        onblur(e) {
-          data.items = [
-            ...data.items,
-            {
-              id: 4,
-              name: e.target.value,
-              description: 'New item',
-              subtitle: 'New item sub',
-            },
-          ]
-          e.target.value = ''
-        },
-      }),
-    ]),
-  ],
-  {
-    data: {
-      amount: 0,
-      items: [
-        {
-          id: 1,
-          name: 'JS',
-          description: 'JavaScript is nice',
-          subtitle: 'JSX',
-        },
-        {
-          id: 2,
-          name: 'Svelte',
-          description: 'Svelte is cool',
-          subtitle: 'SvelteX',
-        },
-        {
-          id: 3,
-          name: 'Praxy',
-          description: 'Praxy is awesome',
-          subtitle: 'PraxyX',
-        },
-      ],
-    },
-  }
-)
-
-const C1 = ({data}) =>
+const Home = () =>
   L(
     'div',
-    [
-      L('p', ['Show']),
-      L('p', [data.name]),
-      L('div', [L('div', [L('span', ['Some span'])])]),
-      L('button', ['Swap name'], {
+    ({data}) => [
+      L('h1', ['Home']),
+      L('button', [data.amount ? `Clicked ${data.amount} times` : 'Click me'], {
         onclick() {
-          data.name = data.name === 'Seb' ? 'Sebastian' : 'Seb'
-        },
-        styles: {
-          background: 'pink',
+          data.amount = data.amount + 1
+          const items = [...data.items]
+          items.splice(0, 1)
+          data.items = items
         },
       }),
-    ],
-    {
-      mounted({el}) {
-        console.log('mounted is good', el)
-      },
-      styles: {
-        color: 'pink',
-        background: 'red',
-        div: {
-          color: 'purple',
-          background: 'grey',
-          span: {
-            background: 'orange',
-            p: {
-              color: 'black',
+      L(
+        'ul',
+        data.items.map((item) =>
+          L(
+            'li',
+            [
+              L('strong', [item.name]),
+              L('p', [item.description], {style: 'margin: 0'}),
+              L('p', [L('span', [item.subtitle])], {
+                style: 'margin: 0',
+              }),
+            ],
+            {
+              class: 'list-item',
+            }
+          )
+        ),
+        {
+          class: 'items-list',
+          styles: {
+            'list-style': 'none',
+            margin: 0,
+            padding: 0,
+            li: {
+              padding: '8px',
+              color: '#333',
+              background: '#eee',
+              'border-radius': '4px',
+              margin: '0 0 4px 0',
             },
           },
-        },
+        }
+      ),
+      L('div', [
+        L('input', [], {
+          type: 'text',
+          onblur(e) {
+            data.items = [
+              ...data.items,
+              {
+                id: 4,
+                name: e.target.value,
+                description: `${e.target.value} title`,
+                subtitle: `${e.target.value} subtitle`,
+              },
+            ]
+            e.target.value = ''
+          },
+        }),
+      ]),
+    ],
+    {
+      data: {
+        amount: 0,
+        items: [
+          {
+            id: 1,
+            name: 'JS',
+            description: 'JavaScript is nice',
+            subtitle: 'JSX',
+          },
+          {
+            id: 2,
+            name: 'Svelte',
+            description: 'Svelte is cool',
+            subtitle: 'SvelteX',
+          },
+          {
+            id: 3,
+            name: 'Praxy',
+            description: 'Praxy is awesome',
+            subtitle: 'PraxyX',
+          },
+        ],
       },
     }
   )
-const C2 = ({data}) => L('div', [L('p', [L('span', [`Hey ${data.lastname}`])])])
 
 const About = L(
   'div',
   ({data}) => [
     L('h1', ['About']),
-    data.show ? C1 : C2,
-    L('button', ['Toggle'], {
+    L('p', [`Hello ${data.name} ${data.lastname}`], {styles: {color: 'red'}}),
+    data.name === 'Seb'
+      ? L('div', ['test 1'], {styles: {background: 'red'}})
+      : L('div', ['test 2'], {styles: {background: 'green'}}),
+    L('button', ['Swap name'], {
       onclick() {
-        data.show = !data.show
+        data.name = data.name === 'Seb' ? 'Sebastian' : 'Seb'
       },
     }),
   ],
@@ -177,7 +142,7 @@ R(
   ],
   {
     fallback: '#home',
-    root: document.querySelector('#app'),
     layout: Layout,
+    root: document.querySelector('#app'),
   }
 )
