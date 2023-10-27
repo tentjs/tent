e882b3d8e80b900f8c7848d89c55486c06c2deb5
sebkolind <sks1993@gmail.com>
2023-10-27 16:09:57 +0200

    remove praxy

--- example/app.js
+++ example/app.js
@@ -1,3 +1,18 @@
-import { Praxy } from '../dist/praxy'
+import {createRouter} from '../dist/else'
+import {Home, About, Layout} from './pages'
 
-export const App = new Praxy()
+createRouter(
+  [
+    {path: '/', component: Home},
+    {path: '/about-us', component: About},
+    {path: '/about-us/:id', component: About},
+    {path: '/some/:id', component: About},
+    {path: '/test/:id', component: About},
+    {path: '/some/:param/:id', component: About},
+  ],
+  {
+    fallback: '/',
+    layout: Layout,
+    root: document.querySelector('#app'),
+  }
+)
