92bd7487f2ef8224d884d2dba9a67a24b66f0948
sebkolind <sks1993@gmail.com>
2023-10-29 19:36:38 +0100

    use function components as routes

--- example/pages/About.js
+++ example/pages/About.js
@@ -1,71 +1,58 @@
 import {e} from '../../dist/else'
 
-const TestProps = e(
-  'div',
-  ({context}) => [
-    e(
-      'div',
-      context.$props.someProp ? `Test ${context.$props.someProp}` : 'Test'
-    ),
-  ],
-  {
-    props: ['someProp'],
-  }
-)
-
-const About = e(
-  'div',
-  ({context}) => [
-    e('h1', 'About'),
-    e(
-      'p',
-      `Hello ${context.name} ${
-        context.$route.params?.id ? context.$route.params.id : ''
-      }`,
-      {
-        styles: {
-          color: 'purple',
-          background: 'yellow',
-          padding: '8px',
-          'border-radius': '4px',
+function About() {
+  return e(
+    'div',
+    ({context}) => [
+      e('h1', 'About'),
+      e(
+        'p',
+        `Hello ${context.name} ${
+          context.$route.params?.id ? context.$route.params.id : ''
+        }`,
+        {
+          styles: {
+            color: 'purple',
+            background: 'yellow',
+            padding: '8px',
+            'border-radius': '4px',
+          },
+        }
+      ),
+      context.name === 'Seb'
+        ? e('div', [e('p', 'test 1')], {
+            key: 'test1',
+            styles: {background: 'purple'},
+          })
+        : e(
+            'div',
+            [
+              e('div', [
+                e('p', 'test 2'),
+                e('span', 'test 2.a'),
+                e('span', 'test 2.b'),
+              ]),
+            ],
+            {
+              key: 'test2',
+              styles: {background: 'green'},
+            }
+          ),
+      e('button', 'Swap name', {
+        onclick() {
+          context.name = context.name === 'Seb' ? 'Sebastian' : 'Seb'
         },
-      }
-    ),
-    context.name === 'Seb'
-      ? e('div', [e('p', 'test 1')], {
-          key: 'test1',
-          styles: {background: 'purple'},
-        })
-      : e(
-          'div',
-          [
-            e('div', [
-              e('p', 'test 2'),
-              e('span', 'test 2.a'),
-              e('span', 'test 2.b'),
-            ]),
-          ],
-          {
-            key: 'test2',
-            styles: {background: 'green'},
-          }
-        ),
-    e('button', 'Swap name', {
-      onclick() {
-        context.name = context.name === 'Seb' ? 'Sebastian' : 'Seb'
-        TestProps.props.someProp = 'yoyo'
+      }),
+    ],
+    {
+      data() {
+        return {
+          show: true,
+          name: 'Seb',
+        }
       },
-    }),
-    TestProps,
-  ],
-  {
-    data() {
-      return {
-        show: true,
-        name: 'Seb',
-      }
-    },
-  }
-)
+    }
+  )
+}
 
 export {About}
--- example/pages/Home.js
+++ example/pages/Home.js
@@ -2,18 +2,20 @@ import {e} from '../../dist/else'
 import {getItems} from '../services/getItems'
 import {List, ListInput} from '../components'
 
-const Home = e('div', ({context}) => [ListInput(context), List(context)], {
-  async onmount({context}) {
-    context.items = await getItems()
-    context.isLoading = false
-  },
-  data() {
-    return {
-      amount: 0,
-      items: [],
-      isLoading: true,
-    }
-  },
-})
+function Home() {
+  return e('div', ({context}) => [ListInput(context), List(context)], {
+    async onmount({context}) {
+      context.items = await getItems()
+      context.isLoading = false
+    },
+    data() {
+      return {
+        amount: 0,
+        items: [],
+        isLoading: true,
+      }
+    },
+  })
+}
 
 export {Home}
