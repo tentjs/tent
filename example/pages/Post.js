e439787f3085718bbd945d40f2cbf345702e781a
sebkolind <sks1993@gmail.com>
2023-10-31 22:21:48 +0100

    example: fadein when post is fetched

--- example/pages/Post.js
+++ example/pages/Post.js
@@ -1,32 +1,43 @@
-import {e} from '../../dist/else'
+import {e, Link} from '../../dist/else'
 import styles from './post.module.css'
 
 function Post() {
+  return e('div', ({context: {post}}) => Children(post), {
+    // `onrouteready` is similar to `onmount` but it's called when the route is ready,
+    // which means that you have access to the route params, as well as `context` and `el`.
+    async onrouteready({context}) {
+      // You should handle errors and loading states as well, this is just a simple example.
+      const req = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${context.$route.params.id}`
+      )
+      context.post = await req.json()
+    },
+    data() {
+      return {post: null}
+    },
+  })
+}
+
+function Children(post) {
+  if (!post) {
+    return []
+  }
   return e(
     'div',
-    ({context: {post}}) =>
-      post
-        ? [
-            e('h1', post.title),
-            e('p', e('em', 'This is a simple example of a post from an API.')),
-            e('p', post.body),
-          ]
-        : [],
-    {
-      // `onrouteready` is similar to `onmount` but it's called when the route is ready,
-      // which means that you have access to the route params, as well as `context` and `el`.
-      async onrouteready({context}) {
-        // You should handle errors and loading states as well, this is just a simple example.
-        const req = await fetch(
-          `https://jsonplaceholder.typicode.com/posts/${context.$route.params.id}`
-        )
-        context.post = await req.json()
-      },
-      data() {
-        return {post: null}
-      },
-      class: styles.post,
-    }
+    [
+      e('h1', post.title),
+      e('p', e('em', 'This is a simple example of a post from an API.')),
+      e('p', post.body),
+      e(
+        'div',
+        [
+          Link({href: `/post/${post.id - 1}`, text: 'Previous'}),
+          Link({href: `/post/${post.id + 1}`, text: 'Next'}),
+        ],
+        {class: styles.nav}
+      ),
+    ],
+    {class: styles.post}
   )
 }
 
