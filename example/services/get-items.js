4de5ac849dfa253cc3b07d32312cd6e85d0db0b8
sebkolind <sks1993@gmail.com>
2023-10-31 18:29:31 +0100

    refactor: early return

--- example/services/get-items.js
+++ example/services/get-items.js
@@ -1,29 +1,24 @@
 export async function getItems() {
   return await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve([
-        {
-          id: 1,
-          name: 'Grocery shopping',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          done: false,
-        },
-        {
-          id: 2,
-          name: 'Clean the house',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          done: false,
-        },
-        {
-          id: 3,
-          name: 'Go to the gym',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          done: false,
-        },
-      ])
-    }, 1500)
+    resolve([
+      {
+        id: 1,
+        name: 'Grocery shopping',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        done: false,
+      },
+      {
+        id: 2,
+        name: 'Clean the house',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        done: false,
+      },
+      {
+        id: 3,
+        name: 'Go to the gym',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        done: false,
+      },
+    ])
   })
 }
