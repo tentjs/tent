8cbaba560f50739f4f6d64db2bf21ef5bf5fd0ff
sebkolind <sks1993@gmail.com>
2023-11-08 23:23:56 +0100

    add async fetching of data

--- example/app.js
+++ example/app.js
@@ -1,12 +1,12 @@
-import {t, mount} from '../dist/else'
-import {getItems} from './services/get-items'
+import { t, mount } from '../dist/else'
+import { getItems } from './services/get-items'
 
 const AnotherComponent = {
   name: 'another-component',
   data: {
     name: 'Poul',
   },
-  view({props}) {
+  view({ props }) {
     return t('p', `My name is ${props.prop1}`)
   },
 }
@@ -23,20 +23,27 @@ const Component = {
       items: getItems(),
     }
   },
-  view({data}) {
-    // this won't "persist" on re-renders
+  view({ data }) {
+    // This won't "persist" on re-renders
     // but can be used to process data or props
     const reverse = data.bar.split('').reverse().join('')
+
+    if (!data.items.length) {
+      return t('div', [
+        t('p', 'Loading...'),
+        t('p', 'Some awesome items...'),
+      ])
+    }
 
     return t('div', [
       t('p', `This is ${data.foo} and ${reverse}`),
       t('div', `Amount of items ${data.items.length}`),
-      t(AnotherComponent, {prop1: data.bar}),
+      t('ul', data.items.map(item => t('li', `${item.name}`))),
+      t(AnotherComponent, { prop1: data.bar }),
       data.test ? t('p', 'Hey') : t('p', 'Hi'),
       t('div', [t('p', 'This is a paragraph in a nested div')]),
       t('button', 'Click me', {
         onclick() {
-          bar = 'boo'
           data.foo = 'something else'
           data.test = 'now i am set'
         },
--- example/services/get-items.js
+++ example/services/get-items.js
@@ -1,24 +1,26 @@
 export async function getItems() {
   return await new Promise((resolve) => {
-    resolve([
-      {
-        id: 1,
-        name: 'Grocery shopping',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        done: false,
-      },
-      {
-        id: 2,
-        name: 'Clean the house',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        done: false,
-      },
-      {
-        id: 3,
-        name: 'Go to the gym',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        done: false,
-      },
-    ])
+    setTimeout(() => {
+      resolve([
+        {
+          id: 1,
+          name: 'Grocery shopping',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          done: false,
+        },
+        {
+          id: 2,
+          name: 'Clean the house',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          done: false,
+        },
+        {
+          id: 3,
+          name: 'Go to the gym',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          done: false,
+        },
+      ])
+    }, 1000)
   })
 }
