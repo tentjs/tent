9763f335aefb9c980fe0e13801c8eb65db13e113
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-11-14 22:17:23 +0100

    update example

--- example/app.js
+++ example/app.js
@@ -2,6 +2,7 @@ import { o, mount } from '../dist/one'
 import { getItems } from './services/get-items'
 
 const Component = {
+  name: 'component',
   data () {
     return {
       items: []
@@ -19,6 +20,7 @@ const Component = {
 }
 
 const List = {
+  name: 'list',
   view ({ props }) {
     return o('div', [
       o('ul',
@@ -30,10 +32,58 @@ const List = {
   }
 }
 
-const View = {
-  view () {
+const If = {
+  name: 'if',
+  data () {
+    return {
+      name: 'John Doe'
+    }
+  },
+  view ({ data }) {
     return o('div', [
-      o(Component)
+      o('h1', 'If'),
+      o('p', 'Paragraph inside If'),
+      o('button', `Click ${data.name}`, {
+        onclick () {
+          console.log('clicked')
+          data.name = 'Jane Doe'
+        }
+      })
+    ])
+  }
+}
+
+const Else = {
+  name: 'else',
+  view ({ props }) {
+    return o('div', [
+      o('h1', 'Else'),
+      o('p', `Paragraph inside Else, with prop "name": ${props.name}`),
+      o('p', 'Another one')
+    ])
+  }
+}
+
+const View = {
+  name: 'view',
+  data () {
+    return {
+      name: 'Sebastian',
+      show: true
+    }
+  },
+  view ({ data }) {
+    return o('div', [
+      o(Component),
+      o('p', `Hello ${data.name}`),
+      data.show
+        ? o(If, null)
+        : o(Else, { name: data.name }),
+      o('button', 'Click to toggle', {
+        onclick () {
+          data.show = !data.show
+        }
+      })
     ])
   }
 }
