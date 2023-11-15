4970ba2dc1afa216d58ec0ebbfae427369a6523b
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-11-16 00:11:07 +0100

    trying out a whole other approach, kind of like it!

--- example/app.js
+++ example/app.js
@@ -1,102 +1,26 @@
-import { o, mount } from '../dist/one'
-import { getItems } from './services/get-items'
+import { O } from '../dist/one'
 
-const Component = {
-  name: 'component',
-  data () {
-    return {
-      items: []
-    }
-  },
-  async onmount ({ data }) {
-    data.items = await getItems()
-  },
-  view ({ data }) {
-    return o('div', [
-      o('h1', 'Items'),
-      o(List, { items: data.items })
-    ])
-  }
-}
+const Component = new O(function () {
+  this.el = '[hello-world]'
+  this.setState({
+    name: 'John Doe',
+    count: 0,
+    text: 'Click me'
+  })
+})
 
-const List = {
-  name: 'list',
-  view ({ props }) {
-    return o('div', [
-      o('ul',
-        props.items.length
-          ? props.items.map(item => o('li', `${item.name}`))
-          : o('li', 'Loading')
-      )
-    ])
-  }
-}
+const Button = new O(function () {
+  this.el = 'button'
+  this.setScope(Component)
 
-const If = {
-  name: 'if',
-  data () {
-    return {
-      count: 0
-    }
-  },
-  onmount () {
-    console.log('If mounted')
-  },
-  view ({ data }) {
-    return o('div', [
-      o('h1', 'If'),
-      o('p', 'Paragraph inside If'),
-      o('button', data.count ? `Clicked ${data.count} times` : 'Click', {
-        onclick () {
-          console.log('clicked')
-          data.count = data.count + 1
-        }
-      })
-    ])
-  }
-}
+  this.on('click', function (data) {
+    const count = data.count + 1
 
-const Else = {
-  name: 'else',
-  view ({ props }) {
-    return o('div', [
-      o('h1', 'Else'),
-      o('p', `Paragraph inside Else, with prop "name": ${props.name}`),
-      o('p', 'Another one')
-    ])
-  }
-}
+    this.setState({
+      count,
+      text: `Clicked ${count}`
+    })
+  })
+})
 
-const View = {
-  name: 'view',
-  data () {
-    return {
-      name: 'Sebastian',
-      show: true
-    }
-  },
-  view ({ data }) {
-    return o('div', [
-      o(Component),
-      o('p', `Hello ${data.name}`),
-      o('input', [], {
-        value: data.name,
-        name: 'input',
-        type: 'text',
-        oninput (e) {
-          data.name = e.target.value
-        }
-      }),
-      data.show
-        ? o(If, null, { redraw: true })
-        : o(Else, { name: data.name }, { redraw: true }),
-      o('button', 'Click to toggle', {
-        onclick () {
-          data.show = !data.show
-        }
-      })
-    ])
-  }
-}
-
-mount(View, document.body)
+console.log('Component', Component, 'Button', Button)
