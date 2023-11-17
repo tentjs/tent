ffa385246eb9741e711fce05711fa1574b2d51d5
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-11-17 23:40:02 +0100

    simplify and focus on simple reactivity

--- example/app.js
+++ example/app.js
@@ -1,83 +1,27 @@
 import { One } from '../dist/one'
 
-const Component = new One(
-  '[hello-world]',
-  async function () {
-    const items = [
-      { id: 1, title: 'one' },
-      { id: 2, title: 'two' },
-      { id: 3, title: 'three' }
-    ]
-
-    this.state = {
-      name: 'John Doe',
+const Test1 = new One(function () {
+  return {
+    state: {
+      msg: 'Hello, World!',
       count: 0,
-      selected: 'two',
-      items,
-      subtitle: getSubtitle(items.length),
-      checked: false,
-      test () {
-        return this.state.selected.toUpperCase()
-      }
-    }
-
-    this.methods = {
-      test: () => {
+      list: [
+        { id: 1, name: 'One' },
+        { id: 2, name: 'Two' },
+        { id: 3, name: 'Three' },
+        { id: 4, name: 'Four' },
+        { id: 5, name: 'Five' }
+      ]
+    },
+    methods: {
+      handleClick: () => {
         this.state = {
-          name: 'Jane Doe'
-        }
-      },
-      test2: (e) => {
-        this.state = {
-          name: e.target.value
+          msg: 'Hello, Europe!',
+          count: this.state.count + 1
         }
       }
     }
-
-    this.computed = {
-      test3 () {
-        return this.state.name.toUpperCase()
-      }
-    }
-
-    // Registering a component inside another component
-    // will make the inner component(s) inherit state, computed, methods, etc.
-    this.register([Button])
   }
-)
+})
 
-const Test1 = new One(
-  '[test-1]',
-  function () {
-    this.state = {
-      title: 'This is the title'
-    }
-  }
-)
 Test1.mount()
-
-const Button = new One(
-  '#btn',
-  function () {
-    this.on('click', function ({ state }) {
-      const count = state.count + 1
-      const items = state.items.filter(i => i.id === 1)
-
-      this.state = {
-        buttonText: `Clicked ${count} times`,
-        count,
-        items,
-        subtitle: getSubtitle(items.length)
-      }
-    })
-  }
-)
-
-Component.mount()
-
-function getSubtitle (count) {
-  const start = count < 2 ? 'is' : 'are'
-  const end = count > 1 ? 'items' : 'item'
-
-  return `There ${start} ${count} ${end}`
-}
