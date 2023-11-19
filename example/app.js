78b75fe142580b08d1ff2aa5a9f106873a293039
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-11-19 13:50:24 +0100

    one fn approach

--- example/app.js
+++ example/app.js
@@ -1,54 +1,17 @@
-import { One } from '../dist/one'
-import { getItems } from './services/get-items'
+import { one } from '../dist/one'
 
-const Test1 = new One(function () {
-  return {
-    name: 'instance1',
-    onmount: async () => {
-      this.state = {
-        list: await getItems()
-      }
-    },
-    state: {
-      msg: 'Hello, World!',
-      count: 0,
-      list: []
-    },
-    computed: {
-      uppercaseMsg: () => {
-        return this.state.msg.toUpperCase()
-      },
-      show: () => {
-        return this.state.count > 2
-      },
-      classNames: () => {
-        return this.state.count > 2 ? 'text-blue-500' : 'text-red-500'
-      },
-      disabled: () => {
-        return this.state.count > 5
-      }
-    },
-    methods: {
-      handleClick: () => {
-        this.state = {
-          msg: 'Hello, Europe!',
-          count: this.state.count + 1,
-          list: this.state.list.filter((item) => item.id !== 1)
-        }
-      }
-    }
+one({
+  name: 'my-component',
+  props: ['msg'],
+  template: `
+    <p o-text="msg">Text</p>
+    <button>Click me</button>
+  `,
+  setup ({ query, click }) {
+    const btn = query('button')
+
+    click(btn, function ({ state }) {
+      state.msg = 'Hello World #2'
+    })
   }
 })
-
-Test1.mount()
-
-const Test2 = new One(function () {
-  return {
-    name: 'instance2',
-    state: {
-      msg: 'Hello, World #2!'
-    }
-  }
-})
-
-Test2.mount()
