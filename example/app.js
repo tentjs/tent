4e51463478e55b52ebecb4b2ce6cd20224c62c68
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-11-19 22:46:07 +0100

    feat: add store

--- example/app.js
+++ example/app.js
@@ -1,10 +1,18 @@
-import { one } from '../dist/one'
+import { one, createStore } from '../dist/one'
+import { getItems } from './services/get-items'
+
+const store = createStore(function () {
+  return {
+    foo: 'bar'
+  }
+})
 
 one({
   name: 'my-component',
   props: ['msg'],
   template: `
     <p o-text="msg">Text</p>
+    <my-button text="My button"></my-button>
     <button>Click me</button>
   `,
   setup ({ query, click }) {
@@ -12,6 +20,24 @@ one({
 
     click(btn, function ({ state }) {
       state.msg = 'Hello World #2'
+    })
+  }
+})
+
+one({
+  store,
+  name: 'my-button',
+  props: ['text'],
+  template: `
+    <button o-text="$store.foo"></button>
+  `,
+  setup ({ click, query }) {
+    const btn = query('button')
+
+    click(btn, async function ({ store }) {
+      const items = await getItems()
+
+      store.set('foo', items[0].title)
     })
   }
 })
