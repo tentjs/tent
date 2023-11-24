e983dee4cda94a76dedcf41b0299fa86d06cb9bd
Sebastian L. K. Sorensen <sks1993@gmail.com>
2023-11-24 10:17:05 +0100

    example: new `for()` signature

--- example/app.js
+++ example/app.js
@@ -27,14 +27,7 @@ const Test = {
 const List = {
   name: 'my-list',
   state: {
-    items: [
-      { id: 1, title: 'List item #1' },
-      { id: 2, title: 'List item #2' },
-      { id: 3, title: 'List item #3' },
-      { id: 4, title: 'List item #4' },
-      { id: 5, title: 'List item #5' },
-      { id: 6, title: 'List item #6' }
-    ]
+    items: []
   },
   template: html`
     <div>
@@ -56,14 +49,14 @@ const List = {
     const btn = query('button')
 
     btn.on('click', function ({ state }) {
-      const id = state.items.length + 1
+      const id = state.items.length
+        ? state.items[state.items.length - 1]?.id + 1
+        : 1
 
-      state.items.push({ id, title: 'foo' })
-      state.items[0].title = 'test #1'
-      state.items.splice(1, 1)
+      state.items = [...state.items, { id, title: `Foo #${id}` }]
     })
 
-    li.for(state.items, 'id', function ({ el, item }) {
+    li.for(['items', 'id'], function ({ el, item }) {
       if (item.id % 2 === 0) {
         el.style.fontWeight = 'bold'
         el.style.color = 'coral'
