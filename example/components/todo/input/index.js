da550e86315f27f7a35d2bf1e7fded02f8142acf
Sebastian L. K. Sorensen <sks1993@gmail.com>
2023-11-27 18:55:42 +0100

    example: input error msg

--- example/components/todo/input/index.js
+++ example/components/todo/input/index.js
@@ -4,24 +4,42 @@ const TodoInput = {
   name: 'todo-input',
   template: html`
     <div>
-      <input type="text" />
-      <button id="add-btn">Add</button>
+      <div>
+        <input type="text" />
+        <button id="add-btn">Add</button>
+      </div>
+      <span o-text="error"></span>
     </div>
   `,
   state: {
     text: '',
+    error: '',
   },
   setup({ query, state, parent }) {
     const input = query('input')
     const btn = query('#add-btn')
+    const error = query('[o-text="error"]')
+
+    const { show, hide } = error.if({ initial: false })
 
     input.on('input', function () {
       state.text = input.value
     })
 
     btn.on('click', function () {
+      if (state.text === '') {
+        state.error = 'Please enter a todo'
+        show()
+
+        return
+      }
+
+      hide()
+
       parent.events.add(state.text)
+
       state.text = ''
+      input.value = ''
     })
   },
 }
