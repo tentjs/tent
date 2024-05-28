72cecc689e5481a726da259567520e0209dcbe73
sebkolind <sks1993@gmail.com>
2024-05-28 15:19:10 +0200

    fix(attr): remove `attr` as helper method
    BREAKING CHANGE: This will remove the `attr` made available when using
    `view` and `mounted`. Instead you can use `el.dataset` and type your
    expected incoming data-* attributes with `Component<{}, Attrs>` and
    define those in `Attrs`.

--- src/attributes.ts
+++ src/attributes.ts
@@ -28,29 +28,4 @@ function addAttribute<A extends Attrs>(
   }
 }
 
-/**
- * @deprecated
- * Use `el.dataset` instead, will be removed in the next major version.
- */
-function getAttribute<A>(el: HTMLElement | Element) {
-  return <K extends keyof A>(name: K): A[K] | undefined => {
-    const attr = el.attributes.getNamedItem(name as string);
-
-    if (!attr) {
-      return;
-    }
-
-    const value = attr.value;
-
-    if (value === '') {
-      // TODO: This might not be the desired behavior.
-      // I should find a better way to handle this,
-      // what I want to avoid is returning `T | undefined | 'true'`
-      return 'true' as A[K];
-    }
-
-    return value as A[K];
-  };
-}
-
-export { addAttribute, getAttribute };
+export { addAttribute };
